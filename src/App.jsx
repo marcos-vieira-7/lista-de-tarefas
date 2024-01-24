fae86f49b157e59a33abcb18673ee93b8a17953f
Marcos Afonso Vieira <marcos.vieira@pelicano.br>
2024-01-24 17:45:37 -0300

    tela de login

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client'
 import TodoList from './TodoList'
 import ListaCompras from './ListaCompras';
 import Menu from './Menu';
+import Login from './Login';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* <TodoList /> */}
     {/* <ListaCompras /> */}
-    <Menu />
+    {/* <Menu /> */}
+    <Login />
   </React.StrictMode>,
 )
